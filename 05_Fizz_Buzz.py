469f42b93db395b316b2ee1bd21bade4e7c4ac0b
alexziel <32820277+alexziel@users.noreply.github.com>
2018-02-01 20:36:07 +0100

    Update Fizz Buzz game

--- 05_Fizz_Buzz.py
+++ 05_Fizz_Buzz.py
@@ -1,4 +1,6 @@
-for i in range(0, 100):
+user_number = int(input('How many times do you want to count? \n'))
+i = 0
+while i < user_number:
     i += 1
     if i % 3 == 0 and i % 5 == 0:
         print('Fizz Buzz')
